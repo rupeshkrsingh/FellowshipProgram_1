0386b3c42d9282ab14671fd2d061d6a93ca4f4d6
rupeshsingh.ags@gmail.com <rupeshsingh.ags@gmail.com>
2022-01-03 19:48:41 +0530

    code updated

---
+++ WriteAndReadJsonDataFromFile/WriteAndReadJsonDataFromFile/PageStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WriteAndReadJsonDataFromFile
+{
+    public class PageStatus
+    {
+        public string PageName { get; set; }
+    }
+}
---
+++ WriteAndReadJsonDataFromFile/WriteAndReadJsonDataFromFile/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace WriteAndReadJsonDataFromFile
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PageStatus status = new PageStatus()
+            {
+                PageName = "RunEventPage"
+            };
+        
+
+            string JsonResult = JsonConvert.SerializeObject(status);
+            string FilePath = "D:\\PageStatus\\EventStatus.txt";
+
+            if (File.Exists(FilePath))
+            {
+                File.Delete(FilePath);
+                using (var tw = new StreamWriter(FilePath, true))
+                {
+                    tw.WriteLine(JsonResult.ToString());
+                    tw.Close();
+                }
+            }
+            else if (!File.Exists(FilePath))
+            {
+                using (var tw = new StreamWriter(FilePath, true))
+                {
+                    tw.WriteLine(JsonResult.ToString());
+                    tw.Close();
+                }
+            }
+
+
+
+            //var jsonText = File.ReadAllText(FilePath);
+            //var EventStatus = JsonConvert.DeserializeObject<PageStatus>(jsonText);
+            //if (EventStatus.PageName=="RunEventPage")
+            //{
+            //    Console.WriteLine("RunEvent Page Enable");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("SuperTracePage Enable");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
